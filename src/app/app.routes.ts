132954fe5380dba951ddcdd8d9bf42e6120831bc
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-06-24 14:16:53 -0300

    router e segunda páigna feitos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LogInComponent} from "./Components/log-in/log-in.component";
+import {SignUpComponent} from "./Components/sign-up/sign-up.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LogInComponent},
+  {path: 'singup', component: SignUpComponent}
+];
